b4e4ab775cb016f17eaf00e9b02d8c5b55f68239
Elijah <eachala5@gmail.com>
2026-01-07 23:47:00 +0000

    3rd commit

--- workflow/nodes.py
+++ workflow/nodes.py
@@ -9,7 +9,7 @@ from workflow.tools import *
 
 
 planner = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.getenv("GROQ_API_KEY"))
-llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.getenv("GAPI"))
+llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.getenv("GROQ_API_KEY"))
 
 
 plannerLlm = planner.with_structured_output(ManagerOuputFormat)
